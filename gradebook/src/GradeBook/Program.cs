3cc22c33453f304ed16c6add16a4a6151df8f12b
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2020-12-25 20:10:09 +0800

    new way of writing arrays

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,12 +6,19 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+
+            var numbers = new double[3];
+            numbers[0] = 12.7;
+            numbers[1] = 13.5;
+            Console.WriteLine(numbers[0]+numbers[1]);
+
             if (args.Length > 0)
             {
-            Console.WriteLine($"Hello {args[0]} !");
+                Console.WriteLine($"Hello {args[0]} !");
             }
-            else {
-            Console.WriteLine("Hello");
+            else
+            {
+                Console.WriteLine("Hello");
             }
             Console.ReadLine();
         }
