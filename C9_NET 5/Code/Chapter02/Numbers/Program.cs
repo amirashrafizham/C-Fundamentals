559a1bd135e4d0bdb84119064913d3d224a54729
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2020-12-26 19:41:31 +0800

    Chapters 1 and 2 initial commit

---
+++ C9_NET 5/Code/Chapter02/Numbers/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // unsigned integer => positive whole number 
+            uint naturalNumber = 23;
+
+            // integer => negative or positive whole number 
+            int integerNumber = -23;
+
+            // float => single-precision floating point.
+            // F suffix => float literal 
+            float realNumber = 2.3F;
+
+            // double => double-precision floating point.
+            double anotherRealNumber = 2.3;
+
+            //Write how you could write the number 2 million
+
+            int decimalNotation = 2_000_000;
+            int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+            int hexadecimalNotation = 0x_001E_8480;
+            // check whether decimalNotation is equals to binaryNotation
+            Console.WriteLine($"{decimalNotation == binaryNotation}");
+
+
+            // Checking the number size of int and double type
+            Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range { int.MinValue:N0} to { int.MaxValue:N0}.");
+            Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range { double.MinValue:N0} to { double.MaxValue:N0}.");
+
+            // Why you shouldn't use double instead of decimal
+            double a = 0.1;
+            double b = 0.2;
+
+            if (a + b == 0.3)
+            {
+                Console.WriteLine($"{a} + {b} is 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"{a} + {b} is not 0.3");
+            }
+
+            decimal c = 0.1m; // M suffix means a decimal literal value
+            decimal d = 0.2m;
+            if (c + d == 0.3m)
+            {
+                Console.WriteLine($"{c} + {d} is 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"{c} + {d} is 0.3");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ C9_NET 5/Code/Chapter02/Variables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object height = 1.88; // storing a double in an object
+            object name = "Amir"; // storing a string in an object
+            Console.WriteLine($"{name} is {height} meters tall. ");
+
+            //int length1 = name.length; // gives compile error!
+            int length2 = ((string)name).Length; // need to tell compiler it's a string
+            Console.WriteLine($"{name} is {length2} characters. ");
+
+
+            dynamic anotherName = "Ahmed"; // storing string in a dynamic object
+            int length = anotherName.Length;
+
+            var population = 66_000_000; // 66 million in UK
+            var weight = 1.88; // in kilograms
+            var price = 4.99M; // in pounds sterling
+            var fruit = "Apples"; // strings use double-quotes
+            var letter = 'Z'; // chars use single-quotes
+            var happy = true; // Booleans have value of true or false
+
+            Console.WriteLine($"default(int) = {default(int)} ");
+            Console.WriteLine($"default(bool) = {default(bool)} ");
+            Console.WriteLine($"default(DateTime) = {default(DateTime)} ");
+            Console.WriteLine($"default(string) = {default(string)} ");
+        }
+    }
+}
